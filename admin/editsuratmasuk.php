e2480d08b1b121891a4fd127b2e573095f73bc4c
Sakila <aerayoon03@gmail.com>
2024-11-24 22:48:13 +0900

    Update editsuratmasuk.php

--- admin/editsuratmasuk.php
+++ admin/editsuratmasuk.php
@@ -113,7 +113,7 @@ include "login/ceksession.php";
                         <label class="control-label col-md-3 col-sm-3 col-xs-12" for="last-name">Nomor Urut <span class="required">*</span>
                         </label>
                         <div class="col-md-9 col-sm-9 col-xs-12">
-                          <input value="<?php echo $data['nomorurut_suratmasuk'];?>" type="text" onkeyup="validAngka(this)" id="nomorurut_suratmasuk" name="nomorurut_suratmasuk" required="required" maxlength="4" placeholder="Masukkan Nomor Urut Surat" class="form-control col-md-7 col-xs-12">
+                          <input value="<?php echo $data['nomorurut_suratmasuk'];?>" type="text" onkeyup="validAngka(this)" id="nomorurut_suratmasuk" name="nomorurut_suratmasuk" required="required" maxlength="5" placeholder="Masukkan Nomor Urut Surat" class="form-control col-md-7 col-xs-12">
                         </div>
                       </div>
                       <div class="form-group">
